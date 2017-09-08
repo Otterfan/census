88bb4081919995bed531926517794dfa0754236f
Benjamin Florin <benjamin.florin@gmail.com>
2017-09-08 18:59:55 -0400

    Cascade place and text deletes

--- app/models/text.rb
+++ app/models/text.rb
@@ -4,14 +4,14 @@ class Text < ApplicationRecord
   belongs_to :topic_author, class_name: 'Person', optional: true
   belongs_to :status
   belongs_to :section, optional: true
-  has_many :text_citations, inverse_of: :text
-  has_many :standard_numbers, inverse_of: :text
-  has_many :components, inverse_of: :text
+  has_many :text_citations, inverse_of: :text, :dependent => :delete_all
+  has_many :standard_numbers, inverse_of: :text, :dependent => :delete_all
+  has_many :components, inverse_of: :text, :dependent => :delete_all
 
-  has_many :other_text_languages, inverse_of: :text
+  has_many :other_text_languages, inverse_of: :text, :dependent => :delete_all
   has_many :languages, :through => :other_text_languages, :class_name => 'Language'
 
-  has_many :publication_places, inverse_of: :text
+  has_many :publication_places, inverse_of: :text, :dependent => :delete_all
   has_many :places, :through => :publication_places, :class_name => 'Place'
 
   accepts_nested_attributes_for :text_citations, :standard_numbers, :components , reject_if: :all_blank, allow_destroy: true
