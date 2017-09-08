4f6f85e55daf6f3c4b7c7fed9ba35fe4cb260ddf
Benjamin Florin <benjamin.florin@gmail.com>
2017-09-08 13:36:15 -0400

    Fix call to non-existant add_illustrator method

--- db/migrate/20170908165547_add_is_illustrated_to_texts.rb
+++ db/migrate/20170908165547_add_is_illustrated_to_texts.rb
@@ -1,9 +1,5 @@
 class AddIsIllustratedToTexts < ActiveRecord::Migration[5.1]
   def change
     add_column :texts, :is_illustrated, :boolean
-
-    reversible do |direction|
-      direction.up {add_is_illustrated}
-    end
   end
 end
