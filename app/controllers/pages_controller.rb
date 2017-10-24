e69f5f8321d85038b09c100f8f7292cbc8560901
Benjamin Florin <benjamin.florin@gmail.com>
2017-10-24 10:41:17 -0400

    Show recently edited texts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @comments = Comment.limit(10).order('id DESC').reverse
+    @comments = Comment.limit(10).order('id DESC')
+    @changed_texts = Text.limit(10).order('updated_at DESC')
   end
 end
