fcdb4bad31e544a829abab054e5435d8dc4ba219
Benjamin Florin <benjamin.florin@gmail.com>
2017-11-10 13:53:33 -0500

    Beginnings of change track pagE

---
+++ app/views/versions/_version.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! version, :id, :item_type, :item_id, :event, :object, :created_at, :object_changes, :created_at, :updated_at
+json.url version_url(version, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :versions, :path => '/changes', :only => [:index, :show]
   resources :comments
   resources :volumes
   resources :journals
