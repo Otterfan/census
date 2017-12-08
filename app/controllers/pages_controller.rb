58412e81e3b0466f5652a8870f7655ffd8d3bd46
Benjamin Florin <benjamin.florin@gmail.com>
2017-12-08 15:10:40 -0500

    Only show changes that affect the visible database. Fixes #57

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
   def index
     @comments = Comment.order(updated_at: :desc).limit(10)
-    @changed_texts = Text.order(updated_at: :desc).limit(10)
+    @changed_texts = PaperTrail::Version.where("item_type = 'Text'").order(:created_at => :desc).limit(10).includes(:item)
   end
 end
