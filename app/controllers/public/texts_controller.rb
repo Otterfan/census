0ef07eeb975d06552706f56781d64f34cdf686e9
Jesse Martinez <mjesse@gmail.com>
2018-01-30 12:36:11 -0500

    adding sort for public title view

--- app/controllers/public/texts_controller.rb
+++ app/controllers/public/texts_controller.rb
@@ -1,9 +1,16 @@
 class Public::TextsController < ApplicationController
+  layout "public"
 
   # GET /public/texts
   # GET /public/texts.json
   def index
     @texts = Text.order(:id).page(params[:page])
+
+    if params[:sort] == "author"
+      @sorted = "author"
+    else
+      @sorted = "title"
+    end
   end
 
   # GET /public/texts/1
