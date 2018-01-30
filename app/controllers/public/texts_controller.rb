8883149f9ca011ab3022a563f194b96e6d8cad47
Jesse Martinez <mjesse@gmail.com>
2018-01-30 00:09:29 -0500

    adding index and show views

--- app/controllers/public/texts_controller.rb
+++ app/controllers/public/texts_controller.rb
@@ -1,2 +1,15 @@
 class Public::TextsController < ApplicationController
+
+  # GET /public/texts
+  # GET /public/texts.json
+  def index
+    @texts = Text.order(:id).page(params[:page])
+  end
+
+  # GET /public/texts/1
+  # GET /public/texts/1.json
+  def show
+    #redirect_to texts_path(@texts)
+    @text = Text.find(params[:id])
+  end
 end
