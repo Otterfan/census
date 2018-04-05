56205cbcee5f32eab050121293761f9cdb2f4a50
Jesse Martinez <mjesse@gmail.com>
2018-04-05 13:47:14 -0400

    adding letter pagination to volumes public view

--- app/controllers/public/volumes_controller.rb
+++ app/controllers/public/volumes_controller.rb
@@ -1,11 +1,28 @@
 class Public::VolumesController < ApplicationController
+  include Public::ControllersHelper
+
   layout "public"
 
   before_action :authenticate_user!
 
   # GET /public/volumes
   def index
-    @volumes = Volume.order(:title).page(params[:page])
+    default_letter = "0-9"
+    @letter = sanitize_letter(params[:letter], default_letter)
+
+    @alpha_params_options = {
+        bootstrap3: true,
+        include_all: false,
+        js: false
+    }
+
+    # @volumes = Volume.order(:title).page(params[:page])
+
+    # the alpha_paginate gem has a bug where it fail if the field contains an empty or blank value.
+    # be sure to filter out empty field values!
+    @volumes, @alpha_params = Volume
+                                  .order(:title)
+                                  .alpha_paginate(@letter, @alpha_params_options){|volume| volume.title}
   end
 
   # GET /public/volumes/1
