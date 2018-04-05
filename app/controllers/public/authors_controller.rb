432bc5eab595ee39cfe3fdd022dfcb8d29588efe
Jesse Martinez <mjesse@gmail.com>
2018-04-05 13:27:56 -0400

    refactoring alpha_pagination code

--- app/controllers/public/authors_controller.rb
+++ app/controllers/public/authors_controller.rb
@@ -1,28 +1,22 @@
 require "alphabetical_paginate"
 
 class Public::AuthorsController < ApplicationController
+  include Public::ControllersHelper
+
   layout "public"
 
   before_action :authenticate_user!
 
   def index
-    if params[:letter].present?
-      if params[:letter].downcase == "all"
-        @letter = "All"
-      else
-        # sanitize by grabbing first letter only
-        # and substitute non-accepted letters with 'A'
-        # https://stackoverflow.com/a/17086441
-        @sanitized = params[:letter][0,1].gsub(/[^A-Za-zΑ-Ωα-ωίϊΐόάέύϋΰήώ]/, 'A')
-        @letter = @sanitized.upcase
-      end
-    else
-      # set default letter to 'A'
-      # TODO: we're assuming there will always be a name that begins with 'A'
-      @letter = "A"
-    end
+    default_letter = "A"
+    @letter = sanitize_letter(params[:letter], default_letter)
 
     # @authors = Person.where(topic_flag: true).order(:full_name).page(params[:page])
+
+    @alpha_params_options = {
+        bootstrap3: true,
+        include_all: false
+    }
 
     # the alpha_paginate gem has a bug where it fail if the field contains an empty or blank value.
     # be sure to filter out empty field values!
@@ -30,7 +24,7 @@ class Public::AuthorsController < ApplicationController
                                   .where(topic_flag: true)
                                   .where.not(full_name: [nil, ''])
                                   .order(:full_name)
-                                  .alpha_paginate(@letter, {bootstrap3: true, include_all: false}){|person| person.full_name}
+                                  .alpha_paginate(@letter, @alpha_params_options){|person| person.full_name}
   end
 
   def show
