04518682bd4da01601f33ccf6df2351649ddcbdb
Benjamin Florin <benjamin.florin@gmail.com>
2018-04-17 11:39:55 -0400

    Fix original update routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
   resources :roles
-  resources :texts, :id => /[\d\.]+/
+  resources :texts do
+    resources :components
+    post 'original', on: :member, to: 'texts#update_original'
+  end
   resources :standard_identifiers
   resources :people, :path => 'authors'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
