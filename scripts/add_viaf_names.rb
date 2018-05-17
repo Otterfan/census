65726e7801587fe1864a17580012ac3548a3547d
Benjamin Florin <benjamin.florin@gmail.com>
2018-05-16 22:54:16 -0400

    Improved alternate author names.

--- scripts/add_viaf_names.rb
+++ scripts/add_viaf_names.rb
@@ -4,19 +4,19 @@ require 'json'
 puts Faraday::VERSION
 
 def format_viaf_name(viaf_name)
-  viaf_name.tr('0-9', '').tr('-', '').tr(',', ' ')
+  viaf_name.gsub(/,? ?\(?\d\d\d\d-?\d*\)?/, '').gsub('/, *\z/', '').strip
 end
 
 def fetch_viaf(conn, url)
   puts url
   res = conn.get(url)
   json = res.body
+  sleep(0.5)
   JSON.parse(json)
 end
 
 def add_viaf_names(conn, person)
-  puts person.full_name
-  alternate_name = ''
+  alt_names = []
   url = person.viaf.tr('http', 'https') + '/'
   viaf_response = fetch_viaf(conn, url)
 
@@ -27,21 +27,35 @@ def add_viaf_names(conn, person)
 
   if viaf_response['ns1:mainHeadings']['ns1:data'].kind_of?(Array)
     viaf_response['ns1:mainHeadings']['ns1:data'].each do |entry|
-      alternate_name << ' ' << format_viaf_name(entry['ns1:text'])
+      name = format_viaf_name(entry['ns1:text'])
+      alt_names |= [name]
     end
   else
-    alternate_name = format_viaf_name(viaf_response['ns1:mainHeadings']['ns1:data']['ns1:text'])
+    name = format_viaf_name(viaf_response['ns1:mainHeadings']['ns1:data']['ns1:text'])
+    alt_names |= [name]
   end
-  alternate_name
+  alt_names
 end
 
 conn = Faraday.new
 conn.headers = {'Accept' => 'application/json'}
 
-Person.where.not(viaf: [nil, '']).find_each do |person|
-  person.alternate_name = add_viaf_names(conn, person)
+Person.where(topic_flag: true).each do |person|
+  alt_names = []
+
+  unless person.viaf.in? [nil, '']
+    alt_names = add_viaf_names(conn, person)
+  end
+
+  person.texts.each do |text|
+    if text.authors_name_from_source
+      name = text.authors_name_from_source.strip
+      alt_names |= [name]
+    end
+  end
+  alt_names.uniq!
+  person.alternate_name = alt_names.join "\n"
   puts person.alternate_name
   person.save
-  sleep(0.5)
 end
 
