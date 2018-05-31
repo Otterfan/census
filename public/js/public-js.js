fdf1b90df1d6a114d699832bcd6f065c42469e12
Jesse Martinez <mjesse@gmail.com>
2018-05-31 11:02:23 -0400

    fix sort script error on new searches

--- public/js/public-js.js
+++ public/js/public-js.js
@@ -1,18 +1,21 @@
-document.querySelector('#select_sort_order').addEventListener("change", function () {
-    var sort_val = document.querySelector('#select_sort_order').value,
-        sort_param = 'sort=' + sort_val,
-        new_search = '';
+var el = document.querySelector('#select_sort_order');
+if (el) {
+    el.addEventListener("change", function () {
+        var sort_val = document.querySelector('#select_sort_order').value,
+            sort_param = 'sort=' + sort_val,
+            new_search = '';
 
-    if (location.search.includes('sort=')) {
-        new_search = location.search.replace(/sort=[^&$]*/i, sort_param);
-    } else {
-        new_search = location.search.concat('&', sort_param)
-    }
+        if (location.search.includes('sort=')) {
+            new_search = location.search.replace(/sort=[^&$]*/i, sort_param);
+        } else {
+            new_search = location.search.concat('&', sort_param)
+        }
 
-    if (new_search.includes('page=')) {
-        new_search = location.search.replace(/page=[^&$]*&?/i, '');
-    }
+        if (new_search.includes('page=')) {
+            new_search = location.search.replace(/page=[^&$]*&?/i, '');
+        }
 
-    location.search = new_search
-});
+        location.search = new_search
+    });
+}
 
