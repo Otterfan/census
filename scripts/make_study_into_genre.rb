866131a4546d243e5d0cf4876f6d7cbe9a83c5db
Benjamin Florin <benjamin.florin@gmail.com>
2019-03-26 01:03:33 -0400

    Capitalize 'study' in genre

--- scripts/make_study_into_genre.rb
+++ scripts/make_study_into_genre.rb
@@ -1,9 +1,4 @@
-Text.where(text_type: 'study_part').each do |text|
-    text.genre = 'study'
-    text.save
-end
-
-Text.where(text_type: 'study_book').each do |text|
-    text.genre = 'study'
+Text.where(genre: 'study').each do |text|
+    text.genre = 'Study'
     text.save
 end
