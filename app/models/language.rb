1a5f3cce25f86eb5106aaddd8edfa2e93bf1e6df
Benjamin Florin <benjamin.florin@gmail.com>
2020-05-15 14:40:07 -0400

    Fix language sorting

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,4 @@
 class Language < ApplicationRecord
   has_many :texts, through: :other_text_languages
-  default_scope { order('ordinal ASC') }
+  default_scope { order('ordinal ASC, name ASC') }
 end
