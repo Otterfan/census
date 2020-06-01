5fd8f4ab82066eaeb86f1754a699b1a4d7a055d5
Benjamin Florin <benjamin.florin@gmail.com>
2020-06-01 18:28:47 -0400

    Add translators/editors names to tombstone

--- app/helpers/public/search_helper.rb
+++ app/helpers/public/search_helper.rb
@@ -1,2 +1,37 @@
 module Public::SearchHelper
+  def translators_from_search(text_citations)
+    translators = []
+    text_citations.each do |citation|
+      if citation.role === 'translator'
+        translators.push(citation.controlled_name + ' (tr.)')
+      end
+    end
+    translators.join('; ')
+  end
+
+  def editors_from_search(text_citations)
+    editors = []
+    text_citations.each do |citation|
+      if citation.role === 'editor'
+        editors.push(citation.controlled_name + ' (ed.)')
+      end
+    end
+    editors.join('; ')
+  end
+
+  def additional_responsibilities_from_search(text_citations)
+    editors = []
+    translators = []
+    text_citations.each do |citation|
+      if citation.role === 'translator'
+        translators.push(citation.controlled_name + ' (tr.)')
+      end
+
+      if citation.role === 'editor'
+        editors.push(citation.controlled_name + ' (ed.)')
+      end
+    end
+    names = translators + editors
+    names.join('; ')
+  end
 end
