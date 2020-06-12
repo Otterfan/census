f1d1d24314958afd05408b6f9c3a27653645aa28
Benjamin Florin <benjamin.florin@gmail.com>
2020-06-12 09:48:30 -0400

    Add available online notice

--- public/js/public-js.js
+++ public/js/public-js.js
@@ -5,7 +5,7 @@ if (el) {
             sort_param = 'sort=' + sort_val,
             new_search = '';
 
-        var sort_input = document.querySelector('input#sort');
+        var sort_input = document.querySelector('#sort');
         sort_input.value = sort_val;
 
         if (location.search.includes('sort=')) {
