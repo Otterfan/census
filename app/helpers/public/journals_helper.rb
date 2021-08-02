da264b14954c78f7bd9da0afd77d682fde8b7b17
Benjamin Florin <benjamin.florin@gmail.com>
2021-08-01 22:02:57 -0400

    Add pages to journal browse

--- app/helpers/public/journals_helper.rb
+++ app/helpers/public/journals_helper.rb
@@ -1,2 +1,23 @@
 module Public::JournalsHelper
+
+  # Print a formatted page span with the appropriate p. or pp.
+  def formatted_page_span(text)
+
+    # No page span? Return nothing
+    unless text.page_span
+      return ''
+    end
+
+    # Already a "p." in the page span? Just return the field value.
+    if text.page_span.include?('p')
+      prefix = ""
+    elsif text.page_span.include?('-') || text.page_span.include?(',')
+      prefix = "pp."
+    else
+      prefix = "p."
+    end
+
+    "#{prefix} #{text.page_span}"
+  end
+
 end
