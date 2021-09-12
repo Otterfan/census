43662a595d96cdb3fe4d695e416f6823d34ce389
Benjamin Florin <benjamin.florin@gmail.com>
2021-09-12 17:48:25 -0400

    Remove 'From' from sort title if not part of title

---
+++ app/models/concerns/text_sort_builders.rb
@@ -0,0 +1,72 @@
+module TextSortBuilders
+
+  extend ActiveSupport::Concern
+
+  included do
+    before_save :calculate_sort_title
+    before_save :calculate_sort_census_id
+    before_save :calculate_sort_page_span
+    before_save :calculate_sort_date
+    before_save :calculate_sort_author
+  end
+
+  def calculate_sort_title
+    self.sort_title = self.title
+    self.sort_title.strip!  # Strip leading and trailing white space.
+
+    # Remove 'From' from the start if it looks like it isn't part of the title.
+    self.sort_title.sub!(/^["'“‘«]?From: */, '')
+    self.sort_title.sub!(/^["'“‘«]?From _ */,'')
+
+    # Remove punctuation.
+    self.sort_title.gsub!(/["'“”‘’«»:_.\[\]]/, '')
+
+    # Remove leading articles
+    self.sort_title.sub!(/^(An? )|(The )/, '')
+
+  end
+
+  def calculate_sort_census_id
+    if self.census_id.blank?
+      self.sort_census_id = ''
+      return
+    end
+
+    section_id, text_id = self.census_id.split('.')
+
+    text_major, text_minor = text_id.split('-')
+
+    text_minor ||= 0
+
+    major = section_id.to_s.rjust(2, "0")
+    text_major = text_major.to_s.rjust(6, "0")
+    text_minor = text_minor.to_s.rjust(6, "0")
+
+    self.sort_census_id = major + text_major + text_minor
+  end
+
+  def calculate_sort_page_span()
+    if self.page_span
+      matches = self.page_span.match /\D*(\d+)/
+      unless matches.nil?
+        self.sort_page_span = matches[1].to_i
+      end
+    end
+  end
+
+  def calculate_sort_date
+    unless self.date.nil?
+      sort_date = self.date[/\d\d\d\d/]
+      self.sort_date = "#{sort_date}-01-01"
+    end
+  end
+
+  def calculate_sort_author
+    self.sort_author = authors_names
+
+    if self.sort_author == ''
+      self.sort_author = editors_names.join('; ')
+    end
+  end
+
+end
--- app/models/text.rb
+++ app/models/text.rb
@@ -5,6 +5,7 @@ class Text < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   include SharedMethods
+  include TextSortBuilders
 
   belongs_to :language, optional: true
   belongs_to :topic_author, class_name: 'Person', optional: true
@@ -33,11 +34,6 @@ class Text < ApplicationRecord
   accepts_nested_attributes_for :components, reject_if: :all_blank, :allow_destroy => true
 
   before_save :default_values
-  before_save :calculate_sort_census_id
-  before_save :calculate_sort_page_span
-  before_save :calculate_sort_date
-  before_save :calculate_sort_title
-  before_save :calculate_sort_author
 
   after_touch {
     puts "Text record '#{self.id}' was touched."
@@ -574,53 +570,6 @@ class Text < ApplicationRecord
       pub_place.place = self.journal.place
       pub_place.text = self
       self.publication_places << pub_place
-    end
-  end
-
-  def calculate_sort_census_id
-    if self.census_id.blank?
-      self.sort_census_id = ''
-      return
-    end
-
-    section_id, text_id = self.census_id.split('.')
-
-    text_major, text_minor = text_id.split('-')
-
-    text_minor ||= 0
-
-    major = section_id.to_s.rjust(2, "0")
-    text_major = text_major.to_s.rjust(6, "0")
-    text_minor = text_minor.to_s.rjust(6, "0")
-
-    self.sort_census_id = major + text_major + text_minor
-  end
-
-  def calculate_sort_page_span()
-    if self.page_span
-      matches = self.page_span.match /\D*(\d+)/
-      unless matches.nil?
-        self.sort_page_span = matches[1].to_i
-      end
-    end
-  end
-
-  def calculate_sort_date
-    unless self.date.nil?
-      sort_date = self.date[/\d\d\d\d/]
-      self.sort_date = "#{sort_date}-01-01"
-    end
-  end
-
-  def calculate_sort_title
-    self.sort_title = self.title.gsub(/["'“”‘’«»:_.\[\]]/, '').sub(/^(An? )|(The )/, '').strip
-  end
-
-  def calculate_sort_author
-    self.sort_author = authors_names
-
-    if self.sort_author == ''
-      self.sort_author = editors_names.join('; ')
     end
   end
 
