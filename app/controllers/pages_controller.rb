405b7d4ec197a216a8470ad3cae6dd38c0debcee
Benjamin Florin <benjamin.florin@gmail.com>
2022-03-27 18:03:43 -0400

    Fix broken guest login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :restrict_access
 
   layout'home', only: [:index]
 
-  def restrict_access
-    redirect_to "/" unless current_user && current_user.user_type != 'viewer'
-  end
 end
