4d81c25efbdd2b7872c0db4a04a7bdb888367e6f
Benjamin Florin <benjamin.florin@gmail.com>
2025-06-16 22:07:15 -0400

    Don't hide unpublished non-topic-authors

---
+++ db/migrate/20250617020447_remove_is_published_from_controlled_names.rb
@@ -0,0 +1,68 @@
+class RemoveIsPublishedFromControlledNames < ActiveRecord::Migration[6.1]
+  def up
+    self.connection.execute "DROP VIEW IF EXISTS controlled_names;"
+
+    self.connection.execute %Q(CREATE OR REPLACE VIEW controlled_names AS
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM text_citations
+         LEFT JOIN people p on TRIM(p.full_name) = text_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM component_citations
+         LEFT JOIN people p on TRIM(p.full_name) = component_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM volume_citations
+         LEFT JOIN people p on TRIM(p.full_name) = volume_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT greek_full_name, full_name as linking_name, greek_full_name, topic_flag, id AS person_link
+FROM people
+WHERE topic_flag IS true
+  AND greek_full_name IS NOT NULL
+  AND greek_full_name != ''
+  AND published = true
+;)
+  end
+
+  def down
+    self.connection.execute "DROP VIEW IF EXISTS controlled_names;"
+
+    self.connection.execute %Q(CREATE OR REPLACE VIEW controlled_names AS
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM text_citations
+         LEFT JOIN people p on TRIM(p.full_name) = text_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM component_citations
+         LEFT JOIN people p on TRIM(p.full_name) = component_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT controlled_name, controlled_name as linking_name, sort_name, topic_flag, p.id AS person_link
+FROM volume_citations
+         LEFT JOIN people p on TRIM(p.full_name) = volume_citations.controlled_name
+WHERE controlled_name != ''
+  AND controlled_name IS NOT NULL
+
+UNION
+SELECT DISTINCT greek_full_name, full_name as linking_name, greek_full_name, topic_flag, id AS person_link
+FROM people
+WHERE topic_flag IS true
+  AND greek_full_name IS NOT NULL
+  AND greek_full_name != ''
+;)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_22_030937) do
+ActiveRecord::Schema.define(version: 2025_06_17_020447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
